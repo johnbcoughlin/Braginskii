6b3042752ebd0efbdcd6c1b334a483fdb3dc8900
Jack Coughlin <johnbc@g3022.hyak.local>
2024-01-16 13:04:53 -0800

    Working D-K Rayleigh Taylor

--- scripts/hybrid_rayleigh_taylor/runsim.jl
+++ scripts/hybrid_rayleigh_taylor/runsim.jl
@@ -5,9 +5,10 @@ using TimerOutputs
 
 d, sim = HybridRayleighTaylor.make_sim_equilibrium_2d(Val(:gpu))
 dt = 1e-2 / 2
-t_end = 3.0
+t_end = 250.0
 TimerOutputs.reset_timer!()
 Braginskii.runsim!(sim, d, t_end, restart_from_latest=false, adaptive_dt=true,
-    initial_dt=dt, writeout_dt=2.0, log=true
+    initial_dt=dt, writeout_dt=2.0, log=true,
+    snapshot_interval_dt=0.1
     )
 TimerOutputs.print_timer()
