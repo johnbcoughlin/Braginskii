c6ef1536917e85b0ea4d778cdeaeb2186b9c945d
Jack Coughlin <jack@johnbcoughlin.com>
2024-02-09 15:34:53 -0800

    Landau damping campaign

---
+++ campaigns/landau_damping_1d1v/slurm_template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=RT-hybrid-vlasov-comparison-arrayjob
+
+#SBATCH --account=amath
+#SBATCH --partition=gpu-rtx6k
+#SBATCH --nodes=1
+#SBATCH --gpus-per-task=1
+#SBATCH --gpus=2
+#SBATCH --cpus-per-task=2
+#SBATCH --array=1-@@NTASKS@@%2
+#SBATCH --mem=20G
+#SBATCH --time=24:00:00
+
+#SBATCH --chdir=@@WORKDIR@@
+#SBATCH --output=@@WORKDIR@@/sims/RT-%a/sim.log
+#SBATCH --error=@@WORKDIR@@/sims/RT-%a/sim.err
+
+module use ~/modulefiles
+module load julia
+
+export OPENBLAS_NUM_THREADS=1
+
+julia --version
+julia --project=../.. --startup-file=no main.jl --run $SLURM_ARRAY_TASK_ID
